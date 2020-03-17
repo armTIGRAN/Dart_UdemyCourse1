30e58727ef19c24ae3581eadbe84611e2381887d
Tigran David <deulin717@gmail.com>
2020-03-17 18:12:49 +0400

    lesson38

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
 
+import "./question.dart";
 // void main(){
 //   runApp(MyApp());
 // }
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+} 
+
+class _MyAppState extends State<MyApp> {
+  int _questionIndex=0;
+
+  void answerQuestion(){
+    setState(() {
+      if(_questionIndex<questions.length-1){
+        _questionIndex+=1;
+      }
+    });
+    print("Answer 1 chosen");
+  }
+
+  var questions = [
+    "What's your favourite color?",
+    "What's your favourite animal?",
+    "What's your favourite film?",
+    "What's your favourite game?"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text("My First App"), ),
         body: Column(children: <Widget>[
-          Text("Question"),
-          RaisedButton(child:Text("Answer1"), onPressed: null),
-          RaisedButton(child:Text("Answer2"), onPressed: null),
-          RaisedButton(child:Text("Answer3"), onPressed: null),
+          Question(questions[_questionIndex]),
+          RaisedButton(child:Text("Answer1"), onPressed: answerQuestion),
+          RaisedButton(child:Text("Answer2"), onPressed: () => print("Answer 2 chosen")),
+          RaisedButton(child:Text("Answer3"), onPressed: (){
+            //some code
+            print("Answer 3 chosen");
+          }),
         ],)
-      ),
+      ),  
     );  
   }
 }
